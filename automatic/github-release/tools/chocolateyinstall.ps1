3b03a217a1e3948a1b4fec9e0dca876e99983a1e
Sam <fertilefields@gmx.ch>
2021-03-14 11:28:47 +0100

    Add installation script
    TODO:
    - Replace [todo] in install script

---
+++ automatic/github-release/tools/chocolateyinstall.ps1
@@ -0,0 +1,24 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$toolsPath = Split-Path $MyInvocation.MyCommand.Definition
+$pp = Get-PackageParameters
+
+$installDir = $toolsPath
+if ($pp.InstallDir)
+{ 
+  $installDir = $pp.InstallDir
+}
+Write-Host "$env:ChocolateyPackageName is going to be installed in '$installDir'"
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName  
+  unzipLocation  = $installDir
+  url            = 'https://github.com/[todo]/releases/download/[todo]'
+  checksum       = '[todo]'
+  checksumType   = 'sha256'
+  url64          = 'https://github.com/[todo]/releases/download/[todo]'
+  checksum64     = '[todo]'
+  checksumType64 = 'sha256'  
+}
+
+Install-ChocolateyZipPackage @packageArgs
