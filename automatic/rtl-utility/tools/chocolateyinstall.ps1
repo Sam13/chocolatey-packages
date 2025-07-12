4ba64f1874383149836b6ec0e4f671df9a033fec
Chocolatey <chocolatey@realdimensions.net>
2025-07-12 16:06:05 +0000

    AU: 1 updated - rtl-utility [skip ci] https://gist.github.com/Sam13/4c58597cdc348acb837256cb29bc897d/abbc56c74bc7290e0dbb24429ade9ee7c0fd5e69

--- automatic/rtl-utility/tools/chocolateyinstall.ps1
+++ automatic/rtl-utility/tools/chocolateyinstall.ps1
@@ -2,11 +2,11 @@
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName  
-  url            = 'https://oblique-audio.com/downloads/RTL_Utility_1_0_11_Win32.exe'
-  checksum       = '5560e6f47700ab1c2e5bb28ac10c21228868bf318dc2b6f0f57046c59fca16d7'
+  url            = 'https://oblique-audio.com/downloads/RTL_Utility_1_0_12_Win32.exe'
+  checksum       = 'de2a5ff3a3949ebaaa69bb83a2976579351b0f2e8e4580526b6c1018bfc216dd'
   checksumType   = 'sha256'
-  url64          = 'https://oblique-audio.com/downloads/RTL_Utility_1_0_11_x64.exe'
-  checksum64     = '23044c531953aa1e4964c700745fbf2c4140390f6f24ce76ffd1b5abe0cec875'
+  url64          = 'https://oblique-audio.com/downloads/RTL_Utility_1_0_12_x64.exe'
+  checksum64     = '0b095799fe3cf3e6055a56cf799eec53e9f5f65f0d3a65ddc77e7b2698a87b47'
   checksumType64 = 'sha256'
 }
 
